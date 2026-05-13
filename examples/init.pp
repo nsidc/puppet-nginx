9e7d07000035390a4bc8f84fc82d749bd3a0b99b
Scott Lewis <scott.lewis@colorado.edu>
2026-05-13 16:58:37 -0600

    Trying to fix error in circle

--- examples/init.pp
+++ examples/init.pp
@@ -3,7 +3,14 @@
 # and the notify last should always come AFTER all resources
 # managed by the nginx class.
 node default {
+  # notify { 'before': }
+  # -> class { 'nginx': }
+  # -> notify { 'last': }
+
   notify { 'before': }
-  -> class { 'nginx': }
-  -> notify { 'last': }
+  notify { 'last': }
+
+  class { 'nginx': }
+
+  Notify['before'] -> Class['nginx'] -> Notify['last']
 }
